3b34b5763104719d402412da27dda31227dc9b85
Mateo Cortes <mateo.cortes1@o365.unab.edu.co>
2022-12-19 18:17:19 -0500

    build: connect and bring backend genres

--- src/pages/main/components/header/index.js
+++ src/pages/main/components/header/index.js
@@ -1,23 +1,45 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
 
 export const Header = () => {
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    getGenresAsync();
+  }, []);
+
+  const getGenresAsync = async () => {
+    let response = await fetch("http://localhost:8080/api/genre");
+    response = await response.json();
+    setGenres(response);
+  };
+
   return (
     <nav>
-
       <div className="logo">
         <h1>FilmHub</h1>
       </div>
 
       <div className="links">
         <a href="#">Home</a>
-        <a href="#">Genres</a>
+      </div>
+
+      <div className="subGenres">
+        <button className="genres">
+          Genres <i className="fa fa-caret-down"></i>
+        </button>
+        <div className="subGenresContent">
+          {genres.map((item, idx) => (
+            <a key={idx} href="#">
+              {item.name}
+            </a>
+          ))}
+        </div>
       </div>
 
       <div className="search">
-        <img></img>
         <input type="text"></input>
       </div>
-
     </nav>
   );
 };
